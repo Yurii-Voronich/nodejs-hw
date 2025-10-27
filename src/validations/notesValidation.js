e9cd2a726e8287e9255d85a0a4a4ef3841e3d018
Yurii <voronich97@gmail.com>
2025-10-27 10:08:39 +0200

    fix

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -33,8 +33,8 @@ export const updateNoteSchema = {
     noteId: Joi.string().custom(objectIdValidator).required(),
   }),
   [Segments.BODY]: Joi.object({
-    title: Joi.string().min(1).required(),
+    title: Joi.string().min(1),
     content: Joi.string().allow(''),
     tag: Joi.string().valid(...TAGS),
-  }),
+  }).min(1),
 };
