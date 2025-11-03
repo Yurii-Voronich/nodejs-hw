071eab7922137a2f245eb057cc12b13288ab43db
Yurii <voronich97@gmail.com>
2025-11-03 11:41:59 +0200

    ready

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,11 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { registerUser, loginUser } from '../controllers/authController.js';
+import {
+  registerUser,
+  loginUser,
+  refreshUserSession,
+  logoutUser,
+} from '../controllers/authController.js';
 import {
   registerUserSchema,
   loginUserSchema,
@@ -9,5 +14,7 @@ import {
 const authRoutes = Router();
 authRoutes.post('/register', celebrate(registerUserSchema), registerUser);
 authRoutes.post('/login', celebrate(loginUserSchema), loginUser);
+authRoutes.post('/refresh', refreshUserSession);
+authRoutes.post('/logout', logoutUser);
 
 export default authRoutes;
